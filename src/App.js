262443babf53165d5d116b40c817ba7c782755a1
Andrew Javidfar <javida1492@gmail.com>
2019-09-02 18:09:26 -0400

    Added category and costname columns

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Expenses from "./components/Expenses";
+import Image from "react-image-resizer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import logo from "./image1.png";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Image
+          height={120}
+          width={720}
+          src={logo}
+          alt={"logo"}
+          style={{ position: "center" }}
+        />
+        <h1>Life Calculator</h1>
+        <div
+          style={{ height: "100px", width: "500px", display: "inline-block" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Expenses />
+        </div>
+        <div
+          style={{ height: "100px", width: "400px", display: "inline-block" }}
+        >
+          <Expenses />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
