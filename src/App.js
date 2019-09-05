f9a72066f2c58dfe5cf0d35c6446e03c0d50f37e
Andrew Javidfar <javida1492@gmail.com>
2019-09-05 17:33:53 -0400

    Static table complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Expenses from "./components/Expenses";
+import Table from "./components/Table";
 import Image from "react-image-resizer";
 
 import logo from "./image1.png";
-
+/*
 class App extends Component {
   render() {
     return (
@@ -15,17 +16,38 @@ class App extends Component {
           alt={"logo"}
           style={{ position: "center" }}
         />
-        <h1>Life Calculator</h1>
+        <h1 style={{textAlign: "center"}}>Life Calculator</h1>
         <div
-          style={{ height: "100px", width: "500px", display: "inline-block" }}
+          style={{ height: "100px", width: "400px", display: "inline-grid", padding: "30px"}}
         >
           <Expenses />
         </div>
         <div
-          style={{ height: "100px", width: "400px", display: "inline-block" }}
+          style={{ height: "100px", width: "400px", display: "inline-grid", padding: "30px" }}
         >
           <Expenses />
         </div>
+      </div>
+    );
+  }
+}
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Logo">
+        <Image
+          height={120}
+          width={750}
+          src={logo}
+          alt={"logo"}
+          style={{ left: "550px" }}
+        />
+        <h1 align="center" style={{ margin: "0 0 2rem 0", fontWeight: "500" }}>
+          Ideal Life Calculator
+        </h1>
+        <Table/>
       </div>
     );
   }
