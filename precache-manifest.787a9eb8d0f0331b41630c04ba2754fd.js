dacb466f0ed0a09d79c1fc0cff6daf5f07b88bfe
Andrew Javidfar <javida1492@gmail.com>
2019-09-11 19:41:20 -0400

    Updates

---
+++ precache-manifest.787a9eb8d0f0331b41630c04ba2754fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b251d2f9ae905a7110e38ea3c7d03b1",
+    "url": "/life-calculator/index.html"
+  },
+  {
+    "revision": "c6947185fb754c22f095",
+    "url": "/life-calculator/static/css/main.94ae7f0f.chunk.css"
+  },
+  {
+    "revision": "9330c46f75230a070bf5",
+    "url": "/life-calculator/static/js/2.6e19c739.chunk.js"
+  },
+  {
+    "revision": "c6947185fb754c22f095",
+    "url": "/life-calculator/static/js/main.f606c581.chunk.js"
+  },
+  {
+    "revision": "3a955af195290f39b88b",
+    "url": "/life-calculator/static/js/runtime~main.df891960.js"
+  },
+  {
+    "revision": "b1c312d406f57035d1528287d70c0795",
+    "url": "/life-calculator/static/media/image1.b1c312d4.png"
+  }
+]);
