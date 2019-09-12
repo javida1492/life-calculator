45123c2fdeda28987cc452c534a4a6fa917dff4b
Andrew Javidfar <javida1492@gmail.com>
2019-09-12 16:33:59 -0400

    Added top border to title. Changed font size of title.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import logo from "./image1.png";
 class App extends Component {
   render() {
     return (
-      <div className="Logo">
+      <div className="Logo" >
         <Image
           height={120}
           width={750}
           src={logo}
           alt={"logo"}
-          style={{marginLeft: "auto", marginRight: "auto"  }}
+          style={{marginLeft: "auto", marginRight: "auto"}}
         />
-        <h1 id="title" align="center" style={{ margin: "0 0 2rem 0", fontWeight: "550", fontSize: "50px" }}>
+        <h1 id="title" align="center" style={{ margin: "0 0 2rem 0", fontWeight: "500", fontSize: "50px", borderTop:"1px solid black"}}>
           Ideal Life Calculator
         </h1>
         <TableCustom />
